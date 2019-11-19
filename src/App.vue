8867b1f0bdf783ab9ac09feb831ffafbe88c7945
rodgeraraujo <rogerio.araujo@mail.com>
2019-11-18 22:58:26 -0300

    Initial project :dizzy:

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo_element.png">
+    <HelloElement msg="Welcome to ElementUI with Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloElement from './components/HelloElement.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloElement
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
